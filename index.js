f8f85e8793d8cfa46d32ef0bf33b3739f26f0d06
Mr Surus <mrsurusmk07@gmail.com>
2022-11-08 14:22:17 +0600

    get service by id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -17,11 +17,24 @@ async function run(){
     try{
         const reviewCollection = client.db('reviewAssign').collection('review')
 
-        app.get('/services', async(req, res)=> {
+        app.get('/servicesh', async(req, res)=> {
+            const query = {}
+            const cursor = reviewCollection.find(query)
+            const result = await cursor.limit(3).toArray()
+            res.send(result)
+        })
+        app.get('/allservices', async(req, res)=> {
             const query = {}
             const cursor = reviewCollection.find(query)
             const result = await cursor.toArray()
             res.send(result)
+        })
+        app.get('/allservices/:id', async(req, res)=> {
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const result =await reviewCollection.findOne(query)
+            res.send(result)
+           
         })
     }
     finally{
