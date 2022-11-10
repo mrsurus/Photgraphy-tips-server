f3fbcd329ce7a0b6c8710ca2f17d184f76cea441
Mr Surus <mrsurusmk07@gmail.com>
2022-11-10 16:16:35 +0600

    query error solve

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nzh9xhl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJwt(req,res,next){
+function verifyJwt(req, res, next) {
 
     const authHeader = req.headers.authorization
-    if(!authHeader){
-        return  res.status(401).send({message: 'unauthorized access'})
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
     }
     const token = authHeader.split(' ')[1]
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
-        if(err){
-           return res.status(401).send({message: 'unauthorixed access'})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorixed access' })
         }
         req.decoded = decoded;
         next()
@@ -58,10 +58,10 @@ async function run() {
 
         })
 
-        app.post('/jwt', (req, res)=> {
+        app.post('/jwt', (req, res) => {
             const user = req.body
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '2h'})
-            res.send({token})
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '2h' })
+            res.send({ token })
         })
 
         app.post('/allservices', async (req, res) => {
@@ -78,10 +78,19 @@ async function run() {
 
         app.get('/review', verifyJwt, async (req, res) => {
             const decoded = req.decoded
-            if(decoded.email !== req.query.email){
-                return res.status(401).send({message: 'unauthorized access'})
+            if (decoded.email !== req.query.email) {
+                return res.status(401).send({ message: 'unauthorized access' })
             }
-            const query = req.query
+            let query = {}
+            if (req.query.email) {
+                query = { email: req.query.email }
+            }
+            console.log(query);
+            const result = await givenReviewCollection.find(query).toArray()
+            res.send(result)
+        })
+        app.get('/reviews', async(req,res)=>{
+            const query= req.query
             const result = await givenReviewCollection.find(query).toArray()
             res.send(result)
         })
